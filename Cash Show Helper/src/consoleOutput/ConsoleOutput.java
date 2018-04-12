6afb095c73ad28eadc2f6ac8e02d63297af52ea2
James Chen <jchen3652@gmail.com>
2018-04-12 02:18:32 -0400

    Fixed Config

--- Cash Show Helper/src/consoleOutput/ConsoleOutput.java
+++ Cash Show Helper/src/consoleOutput/ConsoleOutput.java
@@ -1,6 +1,5 @@
 package consoleOutput;
 
- 
 import java.awt.AWTException;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -17,117 +16,118 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import javax.swing.text.BadLocationException;
- 
+
+import main.Main;
+
 public class ConsoleOutput extends JFrame {
-    /**
-     * The text area which is used for displaying logging information.
-     */
-    private JTextArea textArea;
-     
-    private JButton buttonStart = new JButton("Force Start");
-    private JButton buttonClear = new JButton("Clear");
-     
-    private PrintStream standardOut;
-     
-    public ConsoleOutput() {
-        super("Cash Show Helper");
-         
-        textArea = new JTextArea(50, 10);
-        textArea.setEditable(false);
-        PrintStream printStream = new PrintStream(new CustomOutputStream(textArea));
-         
-        // keeps reference of standard output stream
-        standardOut = System.out;
-         
-        // re-assigns standard output stream and error output stream
-        System.setOut(printStream);
-        System.setErr(printStream);
- 
-        // creates the GUI
-        setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.insets = new Insets(10, 10, 10, 10);
-        constraints.anchor = GridBagConstraints.WEST;
-         
-        add(buttonStart, constraints);
-         
-        constraints.gridx = 1;
-        add(buttonClear, constraints);
-         
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        constraints.gridwidth = 2;
-        constraints.fill = GridBagConstraints.BOTH;
-        constraints.weightx = 1.0;
-        constraints.weighty = 1.0;
-         
-        add(new JScrollPane(textArea), constraints);
-         
-        // adds event handler for button Start
-        buttonStart.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent evt) {
-                String[] nuffin = {};
-            	try {
-					main.Main.main(nuffin);
-				} catch (AWTException | IOException | InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+	/**
+	 * The text area which is used for displaying logging information.
+	 */
+	private JTextArea textArea;
+
+	private JButton buttonStart = new JButton("Force Start");
+	private JButton buttonClear = new JButton("Clear");
+
+	private PrintStream standardOut;
+
+	public ConsoleOutput() {
+		super("Cash Show Helper");
+
+		textArea = new JTextArea(50, 10);
+		textArea.setEditable(false);
+		PrintStream printStream = new PrintStream(new CustomOutputStream(textArea));
+
+		// keeps reference of standard output stream
+		standardOut = System.out;
+
+		// re-assigns standard output stream and error output stream
+		System.setOut(printStream);
+		System.setErr(printStream);
+
+		// creates the GUI
+		setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.insets = new Insets(10, 10, 10, 10);
+		constraints.anchor = GridBagConstraints.WEST;
+
+		add(buttonStart, constraints);
+
+		constraints.gridx = 1;
+		add(buttonClear, constraints);
+
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		constraints.gridwidth = 2;
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1.0;
+		constraints.weighty = 1.0;
+
+		add(new JScrollPane(textArea), constraints);
+
+		// adds event handler for button Start
+		buttonStart.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				Main.watchingScreen = !Main.watchingScreen;
+				if (Main.watchingScreen == true) {
+					System.out.println("resumed");
+				} else {
+					System.out.println("paused");
 				}
-            }
-        });
-         
-        // adds event handler for button Clear
-        buttonClear.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent evt) {
-                // clears the text area
-                try {
-                    textArea.getDocument().remove(0,
-                            textArea.getDocument().getLength());
-                    standardOut.println("Text area cleared");
-                } catch (BadLocationException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-         
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(480, 320);
-        setLocationRelativeTo(null);    // centers on screen
-    }
-     
-    /**
-     * Prints log statements for testing in a thread
-     */
-    private void printLog() {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    System.out.println("Time now is " + (new Date()));
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            }
-        });
-        thread.start();
-    }
-     
-    /**
-     * Runs the program
-     */
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new ConsoleOutput().setVisible(true);
-            }
-        });
-    }
+				System.out.println(Main.watchingScreen);
+			}
+		});
+
+		// adds event handler for button Clear
+		buttonClear.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				// clears the text area
+				try {
+					textArea.getDocument().remove(0, textArea.getDocument().getLength());
+					standardOut.println("Text area cleared");
+				} catch (BadLocationException ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(480, 320);
+		setLocationRelativeTo(null); // centers on screen
+	}
+
+	/**
+	 * Prints log statements for testing in a thread
+	 */
+	private void printLog() {
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					System.out.println("Time now is " + (new Date()));
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException ex) {
+						ex.printStackTrace();
+					}
+				}
+			}
+		});
+		thread.start();
+	}
+
+	/**
+	 * Runs the program
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new ConsoleOutput().setVisible(true);
+			}
+		});
+	}
 }
